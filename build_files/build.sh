f9c7d34bb0007552246b0ed3d1e6ca8f58bd7fc1
Pablo N. Hess <phess@redhat.com>
2025-05-03 02:39:28 -0300

    enable tailscaled, fix nano removal

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 remove nano-default-editor
+dnf5 remove -y nano-default-editor
 dnf5 install -y \
    at \
    btop \
@@ -62,3 +62,4 @@ dnf5 install keybase
 #### Example for enabling a System Unit File
 
 #systemctl enable podman.socket
+systemctl enable tailscaled
