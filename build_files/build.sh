06951bbea0d8cd85cdbc8349cfa201716c1427a9
Pablo N. Hess <phess@redhat.com>
2025-05-03 02:36:00 -0300

    install firefox on the base system

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,47 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 remove nano-default-editor
+dnf5 install -y \
+   at \
+   btop \
+   distrobox \
+   duperemove \
+   firefox \
+   git \
+   gnome-shell-extension-user-theme \
+   gnome-tweaks \
+   iptraf-ng \
+   krb5-workstation \
+   libvirt-daemon \
+   libvirt-daemon-driver-network \
+   libvirt-daemon-driver-nodedev \
+   libvirt-daemon-driver-qemu \
+   libvirt-daemon-driver-storage \
+   nmap \
+   openssl \
+   pam-u2f \
+   pamu2fcfg \
+   qemu-kvm \
+   ramalama \
+   ripgrep \
+   solaar \
+   strace \
+   sysstat \
+   systemd-container \
+   tailscale \
+   tcpdump \
+   tmux \
+   v4l-utils \
+   yaru-theme \
+   vim \
+   vim-default-editor
+
+dnf5 install https://prerelease.keybase.io/keybase_amd64.rpm
+dnf5 install keybase
+
+## TODO: Figure out how to install RPMs from a repo that is only available inside my intranet.
+
 
 # Use a COPR Example:
 #
@@ -21,4 +61,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
